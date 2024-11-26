469f44e8f01ec9647d901c42e33a2265a063ebf4
username05692 <148766458+username05692@users.noreply.github.com>
2024-11-27 01:33:53 +0200

    Add files via upload

---
+++ l5_z1.py
@@ -0,0 +1,9 @@
+n = int(input("n = "))
+print(f"Enter {n} array elements:")
+arr = [int(input()) for _ in range(n)]
+print("Original array: ", arr)
+
+product = 1  # Початкове значення добутку
+for i in range(1, len(arr), 2):  
+    product *= arr[i]
+print("Product of elements with odd indices: ", product)
